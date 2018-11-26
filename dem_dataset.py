987865a33dcfcc590b2373b6e106f18434c583a2
tadasnik <tadas.nik@gmail.com>
2018-11-26 08:48:33 +0000

    australia add state borders, dem datasets, adding plotting routines

--- dem_dataset.py
+++ dem_dataset.py
@@ -1,66 +1,32 @@
-import os
 import glob
-import datetime
-import rasterio
-import numpy as np
 import xarray as xr
 import pandas as pd
-from envdata import Envdata
 
-class DemData(Envdata):
-    def __init__(self, data_path, bbox=None, hour=None):
-        super().__init__(data_path, bbox=bbox, hour=None)
-
-    def spatial_subset(self, dataset, bbox):
-        """
-        Selects data within spatial bbox. bbox coords must be given as
-        positive values for the Northern hemisphere, and negative for
-        Southern. West and East both positive - Note - the method is
-        naive and will only work for bboxes fully fitting in the Eastern hemisphere!!!
-        Args:
-            dataset - xarray dataset
-            bbox - (list) [North, South, West, East]
-        Returns:
-            xarray dataset
-        """
-        dataset = dataset.sel(longitude = slice(bbox[2], bbox[3]))
-        dataset = dataset.sel(latitude = slice(bbox[0], bbox[1]))
-        return dataset
+def dem_tif_to_csv(fname, dem):
+    """
+    Read srtm raster dataset and write to csv
+    """
+    x_arr = xr.open_rasterio(fname)
+    dfr = x_arr.to_dataframe(name=dem)
+    dfr.reset_index(inplace=True)
+    dfr = dfr.drop('band', axis=1)
+    cols = ['lat', 'long', dem]
+    dfr.columns = cols
+    dfr = dfr.round({'lat': 3,
+                     'long': 3,
+                     dem: 3})
+    if dem == 'elevation':
+        dfr['elevaion'] = dfr['elevation'].astype(int)
+    elif dem == 'aspect':
+        dfr['aspect'][dfr['aspect'] < 0] = 0
 
 
-    def dem_tifs_to_netcdf(self, fnames):
-        """
-        Read Chirps rasters in GeoTiff make one xarray dataset and write to netcdf
-        """
-        x_arrs = []
-        for nr, fname in enumerate(fnames):
-            x_arr = xr.open_rasterio(fname)
-            lons = x_arr.x.values
-            lats = x_arr.y.values
-            darr = xr.DataArray(np.squeeze(x_arr.values), coords=[lats, lons],
-                                dims=['latitude', 'longitude'])
-            darr = self.spatial_subset(darr, self.bbox)
-            x_arrs.append(darr)
-        #dataset = xr.Dataset({'chirps': (('time', 'latitude', 'longitude'),
-        #                                                    np.array(ds.values))},
-        #                      coords = {'time': ds.time.values,
-        #                             'latitude': ds.latitude,
-        #                            'longitude': ds.longitude})
-        #dataset[ds_name] = dataset[ds_name].astype(int)
-        #dataset.to_netcdf(os.path.join(self.data_path, ds_name + '_{0}_deg.nc'.format(sp_res)))
-        return x_arrs
-
+    out_fname = 'data/riau_{}_5km.csv'.format(dem)
+    print('writing dataframe to csv file {0}'.format(out_fname))
+    dfr.to_csv(out_fname, index=False)
+    print('finished writing')
 
 if __name__ == '__main__':
-    data_path = '/mnt/data/dem'
-    riau = [3, -2, 99, 104]
-    ch = DemData(data_path, bbox=riau)
-    #for year in range(2008, 2016, 1):
-    #fnames = glob.glob(data_path + '/*.tif')
-    fname = os.path.join(data_path, 'out.tif')
-    #xars = ch.dem_tifs_to_netcdf(fnames)
-    #x_arrs = ch.chirp_tifs_to_netcdf(fnames)
-        #ds = xr.concat(x_arrs, dim = 'time')
-        #ds = ds.to_dataset('chirps')
-        #dfr = ds.to_dataframe()
-        #dfr.to_pickle(os.path.join(data_path, 'chirps_{0}'.format(year)))
+    for dem in ['elevation', 'slope', 'aspect']:
+        fname = glob.glob('data/riau_{0}_5km.tif'.format(dem))[0]
+        dem_tif_to_csv(fname, dem)
